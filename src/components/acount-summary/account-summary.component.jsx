bf1f09b6e98cfe0900a37c383b87ca19195462b4
kdgyekye <kingsleygyeky@gmail.com>
2021-08-21 23:56:13 +0000

    Modified account summaries page to use one single component to map through the list of required summaries

--- src/components/acount-summary/account-summary.component.jsx
+++ src/components/acount-summary/account-summary.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const AccountSummary = ({summary,count}) => {
+const AccountSummary = ({summary,count,icon, iconColor}) => {
     return (
         <div className="col-lg-6 col-md-6">
             <div className="card text-white bg-flat-color-3">
                 <div className="card-body">
                     <div className="stat-widget-five">
-                        <div className="stat-icon dib flat-color-3">
-                            <i className="pe-7s-note2"></i>
+                        <div className={`stat-icon dib flat-color-${iconColor}`}>
+                            <i className={`${icon}`}></i>
                         </div>
                         <div className="stat-content">
                             <div className="text-left dib">
--- src/pages/Dashboard/Dashboard.component.jsx
+++ src/pages/Dashboard/Dashboard.component.jsx
@@ -1,81 +1,29 @@
 import React, {Fragment} from 'react'
 
+import AccountSummary from "../../components/acount-summary/account-summary.component";
+
 const Dashboard = () => {
+
+    const SummaryTypes =  [
+        {count: 35, summary: 'Total Tasks', icon: 'pe-7s-note2', iconColor: 3},
+        {count: 15, summary: 'Total Categories', icon: 'pe-7s-menu', iconColor: 2},
+        {count: 25, summary: 'Total Completed Tasks', icon: 'pe-7s-check', iconColor: 1},
+        {count: 10, summary: 'Total Uncompleted Tasks', icon: 'pe-7s-less', iconColor: 4}
+    ]
     return (
         <Fragment>
             <div className='animated-fadeIn'>
                 <div className="row">
-                    <div className="col-lg-6 col-md-6">
-                        <div className="card text-white bg-flat-color-3">
-                            <div className="card-body">
-                                <div className="stat-widget-five">
-                                    <div className="stat-icon dib flat-color-3">
-                                        <i className="pe-7s-note2"></i>
-                                    </div>
-                                    <div className="stat-content">
-                                        <div className="text-left dib">
-                                            <div className="stat-text">$<span className="count">35</span></div>
-                                            <div className="stat-heading">Total Tasks</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6 col-md-6">
-                        <div className="card text-white bg-flat-color-2">
-                            <div className="card-body">
-                                <div className="stat-widget-five">
-                                    <div className="stat-icon dib flat-color-2">
-                                        <i className="pe-7s-menu"></i>
-                                    </div>
-                                    <div className="stat-content">
-                                        <div className="text-left dib">
-                                            <div className="stat-text"><span className="count">15</span></div>
-                                            <div className="stat-heading">Categories</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6 col-md-6">
-                        <div className="card text-white bg-flat-color-1">
-                            <div className="card-body">
-                                <div className="stat-widget-five">
-                                    <div className="stat-icon dib flat-color-1">
-                                        <i className="pe-7s-check"></i>
-                                    </div>
-                                    <div className="stat-content">
-                                        <div className="text-left dib">
-                                            <div className="stat-text"><span className="count">25</span></div>
-                                            <div className="stat-heading">Completed</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6 col-md-6">
-                        <div className="card text-white bg-flat-color-4">
-                            <div className="card-body">
-                                <div className="stat-widget-five">
-                                    <div className="stat-icon dib flat-color-4">
-                                        <i className="pe-7s-less"></i>
-                                    </div>
-                                    <div className="stat-content">
-                                        <div className="text-left dib">
-                                            <div className="stat-text"><span className="count">10</span></div>
-                                            <div className="stat-heading">Uncompleted Tasks</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        SummaryTypes.map((type,index) =>(
+                            <AccountSummary
+                                summary={type.summary}
+                                count={type.count}
+                                icon={type.icon}
+                                iconColor={type.iconColor}
+                            />
+                        ) )
+                    }
                 </div>
             </div>
         </Fragment>
