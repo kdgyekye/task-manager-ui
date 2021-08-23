229accf01343119df413a92dbd2514da1d0c7d21
kdgyekye <kingsleygyeky@gmail.com>
2021-08-23 23:45:17 +0000

    Building out tasks component

--- src/pages/Tasks/Tasks.component.jsx
+++ src/pages/Tasks/Tasks.component.jsx
@@ -3,7 +3,58 @@ import React, {Fragment} from "react";
 const Tasks  = () => {
     return (
         <Fragment>
-            <p>Tasks</p>
+            <div className="content">
+                <div className="animated fadeIn">
+                    <div className="row">
+
+                        <div className="col-md-12">
+                            <div className="card">
+                                <div className="card-header">
+                                    <strong className="card-title">Data Table</strong>
+                                </div>
+                                <div className="card-body">
+                                    <table id="bootstrap-data-table" className="table table-striped table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Subject</th>
+                                            <th>Created On</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>$320,800</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Garrett Winters</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>$170,750</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ashton Cox</td>
+                                            <td>Junior Technical Author</td>
+                                            <td>San Francisco</td>
+                                            <td>$86,000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Cedric Kelly</td>
+                                            <td>Senior Javascript Developer</td>
+                                            <td>Edinburgh</td>
+                                            <td>$433,060</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
