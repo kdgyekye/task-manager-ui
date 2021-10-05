a3f833df60c6573ad0bda611fe233ece49ea8413
kdgyekye <kingsleygyeky@gmail.com>
2021-10-05 20:52:00 +0000

    Added sigin route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, {Fragment} from "react";
+import React, {Fragment, Suspense, lazy} from "react";
+import {
+    Route,
+    Switch
+} from "react-router-dom";
 import './App.css';
 import './styles/output.css'
 
 //Component Imports
 import Sidebar from "./components/Sidebar/Sidebar.component";
 import Layout from "./shared/Layout/Layout.component";
+import SignIn from "./pages/Signin/signin.component";
 
 function App() {
   return (
     <div className="App" style={{backgroundColor: 'whitesmoke'}}>
-        <Sidebar />
-        <Layout />
+        <Switch>
+            <Route
+                exact={true}
+                path='/signin'
+                component={SignIn}
+            />
+            {/*<Route*/}
+            {/*    exact={true}*/}
+            {/*    path='/login'*/}
+            {/*    component={LogIn}*/}
+            {/*/>*/}
+            <Route
+                path='*'
+                render={() => (
+                    <Fragment>
+                        <Sidebar />
+                        <Layout />
+                    </Fragment>
+                    )}
+            />
+        </Switch>
+
     </div>
   );
 }
---
+++ src/pages/Signin/signin.component.jsx
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+const SignIn = props => {
+    return(
+        <Fragment>
+            Sigin Page
+        </Fragment>
+    )
+}
+
+export default SignIn
